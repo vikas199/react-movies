ffca18e197ebb6840d3d29b0fcaeab089a43038e
vikas199 <mvikasraj123@gmail.com>
2018-03-06 08:23:02 -0800

    added pagination that display results according to the max results

--- src/containers/books/search.js
+++ src/containers/books/search.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import SearchList from './searchList'
 import * as actions from '../../actions/booksActions'
-//import Pagination from './Pagination'
+import Paginations from './Pagination'
+import {Pagination, PaginationItem, PaginationLink} from 'reactstrap'
 import { connect } from 'react-redux';
 import './search.css'
+
+
+
+let prev=0;
+let next=0;
+let last=0;
+let first=0;
 
 class Search extends Component {
     state = {
         startIndex: 0,
-        maxResults: 40,
+        maxResults: 5,
         currentPage: 1,
         booksPerPage: 5
     }
@@ -16,31 +24,41 @@ class Search extends Component {
        this.props.onInputChange(event.target.value)
     }
 
-    handlePageChange = event => {
+    handlePageChange = (event) => {
         this.setState({ currentPage: Number(event.target.id)})
     }
+    handleLastClick(event){
+        event.preventDefault();
+        this.setState({currentPage: last})
+    }
+    handleFirstClick = (event) => {
+        event.preventDefault();
+        this.setState({currentPage: 1})
+    }
 
-    // handleResults = (event) => {
-    //     this.setState({ maxResults: event.target.value })
+    handleResults = (event) => {
+        this.setState({ maxResults: event.target.value })
 
-    // }
+    }
 
     handleSubmit = (event) => {
         event.preventDefault()
         this.props.books_fetch(this.props.searchTerm, this.state.startIndex, this.state.maxResults);
     }
 
-    // componentDidUpdate(prevProps, prevState){
-    //        if(this.state.maxResults !== prevState.maxResults){
-    //           this.props.books_fetch(this.props.searchTerm, this.state.startIndex, this.state.maxResults)
-    //        }
-    // }
+    componentDidUpdate(prevProps, prevState){
+           if(this.state.maxResults !== prevState.maxResults){
+              this.props.books_fetch(this.props.searchTerm, this.state.startIndex, this.state.maxResults)
+           }
+    }
     render() {
-        console.log(this.state.cu)
         const { currentPage, booksPerPage } = this.state;
         const indexOfLastBook = currentPage *  booksPerPage;
         const indexOfFirstBook = indexOfLastBook - booksPerPage;
         const currentBooks = this.props.books.slice(indexOfFirstBook, indexOfLastBook)
+        prev = currentPage > 0 ? (currentPage -1) : 0;
+        last = Math.ceil(this.props.books.length / booksPerPage)
+        next = (last === currentPage) ? currentPage: currentPage+1
         console.log(this.props.books)
         let bookDetails = null;
         if (this.props.books) {
@@ -55,14 +73,14 @@ class Search extends Component {
 
         const renderPageNumbers = pageNumbers.map(number => {
             return(
-                <li key={number} id={number} onClick={this.handlePageChange} 
-                className="page-item">{number}</li>
+               <li>{number}</li>
             )
         })
         return (
             <div>
                 <div className="searchList">
                     <input onChange={this.handleChange} />
+                    <Paginations onChange={this.handleResults}/>
                     <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>Submit</button>
                 </div>
                 {bookDetails}
